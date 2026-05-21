4e880bd8976360058bd831650f1ba67c3fc41cd4
deepshah103 <77850581+deepshah103@users.noreply.github.com>
2026-05-21 20:14:38 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio_2' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio_2/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio_animated' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio_animated/' : '',
   images: { unoptimized: true },
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei', '@react-three/postprocessing'],
   webpack: (config) => {
