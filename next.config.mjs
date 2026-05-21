81faecffdc1e169c46c9267423bcd19a0c459436
deepshah103 <77850581+deepshah103@users.noreply.github.com>
2026-05-21 20:12:21 +0530

    git deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio_2' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio_2/' : '',
+  images: { unoptimized: true },
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei', '@react-three/postprocessing'],
   webpack: (config) => {
     config.externals = [...(config.externals || []), { canvas: 'canvas' }];
